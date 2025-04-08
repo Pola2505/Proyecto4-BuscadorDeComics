aebb8df59c980671ead6c87c0fb24624501050b0
Sandra <sandra27popielarczyk@gmail.com>
2025-04-07 21:04:45 -0300

    corrigiendo la tarjeta para que sea un link

--- main.js
+++ main.js
@@ -11,15 +11,18 @@ const pintarDatos = (arrayCharacters) => {
     $containerCharacters.innerHTML = '';
   
     for (const character of arrayCharacters) {
-      $containerCharacters.innerHTML += `
-        <div class="w-[30%] bg-white border p-4 rounded-2xl shadow-lg hover:shadow-cyan-600 transition duration-300 ease-in-out hover:scale-105 mb-5">
-          <img class="mx-auto rounded-lg mb-4" src="${character.image}" alt="${character.name}" />
-          <h2 class="text-xl font-semibold text-gray-800 mb-1">Nombre: <span class="text-cyan-600">${character.name}</span></h2>
-          <h3 class="text-gray-600 mb-1">Status: <span class="font-medium">${character.status}</span></h3>
-          <h3 class="text-gray-600 mb-1">Género: <span class="font-medium">${character.gender}</span></h3>
-          <h3 class="text-gray-600">Especie: <span class="font-medium">${character.species}</span></h3>
-        </div>
+        $containerCharacters.innerHTML += `
+        <a id="link-character" href="https://google.com" class="block w-[30%] mb-5">
+          <div class="bg-white border p-4 rounded-2xl shadow-lg hover:shadow-cyan-600 transition duration-300 ease-in-out hover:scale-105">
+            <img class="mx-auto rounded-lg mb-4" src="${character.image}" alt="${character.name}" />
+            <h2 class="text-xl font-semibold text-gray-800 mb-1">Nombre: <span class="text-cyan-600">${character.name}</span></h2>
+            <h3 class="text-gray-600 mb-1">Status: <span class="font-medium">${character.status}</span></h3>
+            <h3 class="text-gray-600 mb-1">Género: <span class="font-medium">${character.gender}</span></h3>
+            <h3 class="text-gray-600">Especie: <span class="font-medium">${character.species}</span></h3>
+          </div>
+        </a>
       `;
+      
     }
   }
   
